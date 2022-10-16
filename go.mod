c7061d7aed87ff649c235bcdccc80a31169450b0
Yoga Baskoro <yoga.baskoro@ottencoffee.co.id>
2022-10-16 10:56:14 +0700

    implement say-hello module

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/yogasab/go-app-say-hello
+
+go 1.19
+
+require github.com/yogasab/go-say-hello v1.0.0 // indirect
---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"log"
+
+	sayHello "github.com/yogasab/go-say-hello"
+)
+
+func main() {
+	log.Println(sayHello.SayHello())
+}
